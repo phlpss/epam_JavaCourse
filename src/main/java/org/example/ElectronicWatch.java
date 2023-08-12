500488ccdfd588ed370cabf4e312ff8fd8e632d9
Katya Filip <philipkatya13@gmail.com>
2023-08-12 14:38:19 +0300

    complete task 2, convert seconds to electronic clock format

---
+++ src/main/java/org/example/ElectronicWatch.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ElectronicWatch {
+    public static void main(String[] args) {
+        // 89999
+        Scanner scanner = new Scanner(System.in);
+        int inputSeconds = scanner.nextInt();
+
+        int hours = inputSeconds / 3600;
+        if (hours == 24) {
+            hours = 0;
+        }
+        int minutes = (inputSeconds % 3600) / 60;
+        int seconds = inputSeconds % 60;
+
+        String formattedTime = String.format("%d:%02d:%02d", hours, minutes, seconds);
+        System.out.println(formattedTime);
+    }
+}
