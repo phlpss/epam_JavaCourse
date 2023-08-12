5a7676da26bbb78b3edebe884206c59b60f72720
Katya Filip <philipkatya13@gmail.com>
2023-08-12 15:15:37 +0300

    complete task 3, program checks input against a secret password

---
+++ src/main/java/org/example/MeetAnAgent.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+// Asks for an input number
+// If the input equals to the secret password number, prints "Hello, Agent"
+// Otherwise, prints "Access denied"
+// Secret password is stored in final static int PASSWORD.
+public class MeetAnAgent {
+    final static int PASSWORD = 133976;
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int inputPassword = in.nextInt();
+
+        if (inputPassword == PASSWORD) {
+            System.out.println("Hello, Agent");
+        } else {
+            System.out.println("Access denied");
+        }
+    }
+}
