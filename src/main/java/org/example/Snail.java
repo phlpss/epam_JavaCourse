7ddc9ec060c37b82b3e39da93b1bc20e2366c3f7
Katya Filip <philipkatya13@gmail.com>
2023-08-12 21:06:30 +0300

    complete task 5, program calculates days for snail to reach tree top

---
+++ src/main/java/org/example/Snail.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+// Consider a snail travels up a tree a feet each day.
+// Then snail slides down b feet each night.
+// Height of the tree is h feet.
+// Write a program that prints number of days for the snail to reach the top of the tree.
+// Program reads a, b, h line by line. Input values are guaranteed to be positive integers.
+// If the snail cannot reach the top of the tree, print the message Impossible.
+
+public class Snail {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt(); // distance climbed during the day
+        int b = in.nextInt(); // distance slid down during the night
+        int h = in.nextInt(); // height of the tree
+
+        if (a >= h) {
+            System.out.println("1");
+        } else if (a <= b && a <= h) {
+            System.out.println("Impossible");
+        } else {
+            int days = (h - b - 1) / (a - b) + 1;
+
+            System.out.println(days);
+        }
+        in.close();
+    }
+}
+
