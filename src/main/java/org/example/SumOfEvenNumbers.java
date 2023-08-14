cfbdaa6bbf030df8283a7be91cd4cc9b796530ac
Katya Filip <philipkatya13@gmail.com>
2023-08-14 17:11:09 +0300

    complete task 11, method sum() calculates sum of even numbers in array

---
+++ src/main/java/org/example/SumOfEvenNumbers.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.stream.IntStream;
+
+public class SumOfEvenNumbers {
+    public static void main(String[] args) {
+        int[] vals = new int[]{1, 3, 2, 8, 198, 15};
+        System.out.println(sum(new int[0]));
+    }
+
+    // return the sum of even numbers in array
+    public static int sum(int[] array) {
+        if(array == null || array.length == 0) {
+            return 0;
+        }
+        int sum = 0;
+        for(int i : array) {
+            if(i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
