060eae0041141ebd6adf7d557f95281e5b40260b
Katya Filip <philipkatya13@gmail.com>
2023-08-14 16:39:22 +0300

    complete task 10, method max() gets max element in array

---
+++ src/main/java/org/example/MaxMethod.java
@@ -0,0 +1,14 @@
+package org.example;
+public class MaxMethod {
+    public static void main(String[] args) {
+        int[] values = new int[]{806, 19, 140, 392, 36, 528, 542, 890, 424};
+        System.out.println(MaxMethod.max(values));
+    }
+    public static int max(int[] array) {
+        int max = Integer.MIN_VALUE;
+        for(int i : array) {
+            if (i > max) max = i;
+        }
+        return max;
+    }
+}
